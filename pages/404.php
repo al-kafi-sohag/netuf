9822211e102722271c25a9939adfa304b1531986
al-kafi-sohag <aksohag16@gmail.com>
2025-03-19 13:33:13 +0600

    Update 404.php

---
+++ pages/404.php
@@ -0,0 +1,15 @@
+    <!-- Page Header Start -->
+    <div class="page-header parallaxie">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-lg-12">
+                    <!-- Page Header Box Start -->
+                    <div class="page-header-box text-center">
+                        <h1 class="wow fadeInUp" data-cursor="-opaque">404 Page Not Found</h1>
+                    </div>
+                    <!-- Page Header Box End -->
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Page Header End -->
